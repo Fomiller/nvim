18049445a5e2ba259b30d3cabdd868e62564cba0
Forrest Miller <forrestmillerj@gmail.com>
2022-12-18 09:43:52 -0600

    comment key bindings and save quit keybindings; lua global varibale 'vim' config with lsp

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -1,9 +1,18 @@
 local lsp = require('lsp-zero')
-
 lsp.preset('recommended')
+lsp.nvim_workspace({
+    settings = {
+        Lua = {
+            diagnostics = {
+                globals = {'vim'}
+            }
+        }
+    }
+})
 lsp.setup()
 
---
+-- THEPRIMEAGENS LSP SETUP
+
 -- lsp.preset("recommended")
 --
 -- lsp.ensure_installed({
